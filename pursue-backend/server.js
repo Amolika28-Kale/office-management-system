1897b4ba92f2e8ddf7e8856a48ebbebf4bb5e201
Amolika28-Kale <amolikakale234@gmail.com>
2026-01-02 02:47:42 +0530

    added cors

--- pursue-backend/server.js
+++ pursue-backend/server.js
@@ -7,7 +7,13 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://pursue-co-working-space.netlify.app"
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
